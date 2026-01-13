09887e18d00b5bb6b3460f0b8cfd46026c776472
travislevine <sneakyplutosaturn@gmail.com>
2026-01-13 14:00:25 +1000

    CH2 L5: Refactor main.py, extract text_word_count to stats.py then import into main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+from stats import text_word_count
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-def text_accept(text):
-    str_text = text.split()
-    return f"Found {len(str_text)} total words"
+
 
 def main():
     print()
-    print(text_accept(get_book_text("/home/pepso/workspace/bootdotdev/bookbot/books/frankenstein.txt")))
+    print(text_word_count(get_book_text("/home/pepso/workspace/bootdotdev/bookbot/books/frankenstein.txt")))
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def text_word_count(text):
+    str_text = text.split()
+    return f"Found {len(str_text)} total words"
