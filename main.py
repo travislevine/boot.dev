dd95c1596e3c379e20a61372389b7e8607f2c318
travislevine <sneakyplutosaturn@gmail.com>
2026-01-13 13:55:15 +1000

    Bookbot CH2 L4: Implement word counter

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ def get_book_text(filepath):
         file_contents = f.read()
     return file_contents
 
+def text_accept(text):
+    str_text = text.split()
+    return f"Found {len(str_text)} total words"
+
 def main():
-    print(get_book_text("/home/pepso/workspace/bootdotdev/bookbot/books/frankenstein.txt"))
+    print()
+    print(text_accept(get_book_text("/home/pepso/workspace/bootdotdev/bookbot/books/frankenstein.txt")))
 
 main()
