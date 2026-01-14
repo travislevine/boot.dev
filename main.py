4ad054ddeac718648aaa82b1561b9420f3c2317f
travislevine <sneakyplutosaturn@gmail.com>
2026-01-14 20:57:31 +1000

    CH3 L2: Add CLI argument handling for dynamic book paths

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_num_words,
     chars_dict_to_sorted_list,
@@ -6,12 +7,16 @@ from stats import (
 
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print_report(book_path, num_words, chars_sorted_list)
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        chars_dict = get_chars_dict(text)
+        chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+        print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path):
